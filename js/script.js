257d1ba14e21dd7ad041d3ef53aaa6f339b355a1
Sushovan Majhi <sush@smajhi.com>
2021-05-05 13:38:51 -0500

    add sol

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $( document ).ready(function() {
     $('.ui.accordion').accordion();
     $('.ui.dropdown').dropdown();
-    $('table').addClass('ui very basic collapsing celled table');  
+    $('table').addClass('ui very basic collapsing celled centered table');  
 });
