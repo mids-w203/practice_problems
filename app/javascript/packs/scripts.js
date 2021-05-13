9817d33c3441f63ee6b72adb8f293813b798812d
Sushovan Majhi <sush@smajhi.com>
2021-05-13 17:31:45 -0500

    add css, js

---
+++ app/javascript/packs/scripts.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    $('.ui.accordion').accordion({
+        selector: {
+            trigger: '.title button'
+        }
+    });
+
+    $('.ui.dropdown').dropdown();
+    $('table').addClass('ui very basic celled centered table');  
+});
