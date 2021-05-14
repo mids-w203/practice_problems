da90c34975add1bc8e9036b330c63849c1346dda
Sushovan Majhi <sush@smajhi.com>
2021-05-14 17:04:53 -0500

    add

--- app/javascript/packs/scripts.js
+++ app/javascript/packs/scripts.js
@@ -1,12 +1,4 @@
 $( document ).on('turbolinks:load',function() {
-    $('.ui.accordion').accordion({
-        selector: {
-            trigger: '.title button'
-        }
-    });
-
     $('.ui.dropdown').dropdown();
-    $('table').addClass('ui very basic celled centered table');  
-
-
+    $('table').addClass('ui very basic celled centered table');
 });
--- app/models/problem.rb
+++ app/models/problem.rb
@@ -1,7 +1,9 @@
 class Problem < ApplicationRecord
   belongs_to :topic
   validates :statement, presence: true
-
+  validates :index, presence: true
+  default_scope { order(index: :asc) }
+  
   def like
     update_attribute(:likes, (likes || 0) + 1)
   end
