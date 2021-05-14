69e4d69a87f54c5d5260381a4c3b75916c0c3a61
Sushovan Majhi <sush@smajhi.com>
2021-05-13 20:11:44 -0500

    add categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,21 +1,41 @@
 class CategoriesController < ApplicationController
+  def index
+    @categories = Category.all
+  end
+
   def show
   end
   
   def create
     @category = Category.new(category_params)
     if @category.save
-      redirect_to root_url
+      redirect_to categories_url
     else
       render 'new'
     end
   end
 
   def edit
+    @category = Category.find(params[:id]) 
   end
   
   def new
     @category = Category.new
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to categories_url
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_url
   end
 
   private
