161fad24e1908bf892874c471fecdc2b424435da
Sushovan Majhi <sush@smajhi.com>
2021-05-16 10:19:08 -0500

    add

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,6 @@
 class Topic < ApplicationRecord
   belongs_to :category
   has_many :problems, dependent: :destroy
-  validates :title presence: true
+  validates :title, presence: true
   default_scope { order(index: :asc) }
 end
