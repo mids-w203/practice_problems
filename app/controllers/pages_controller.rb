df9b305386c06e000175e6abf7508988158f3881
Sushovan Majhi <sush@smajhi.com>
2021-06-13 16:39:21 -0500

    custom render

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
-    before_action :logged_in_user
+    before_action :logged_in_user, except: [:ping]
+
+    def preview
+        @text = params[:text]
+        render inline: "<%= markdown(@text) %>"
+    end
 
     def index
         @categories = Category.all 
--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,5 @@ Rails.application.routes.draw do
   end
   root to: 'pages#index'
   get 'ping', to: 'pages#ping' 
+  post 'preview', to: 'pages#preview'
 end
