bf7d82550a2fa085ecaf415799446f4f10013305
Sushovan Majhi <sush@smajhi.com>
2021-07-27 14:30:08 -0500

    add concepts modal

--- app/javascript/packs/scripts.js
+++ app/javascript/packs/scripts.js
@@ -2,6 +2,11 @@ $( document ).on('turbolinks:load',function() {
     $('.ui.accordion')
         .accordion()
     ;
+    $('.concepts.button').click(function(){
+        $('.ui.concepts.modal')
+         .modal('show')
+        ;
+    });
     $('.ui.dropdown').dropdown();
     $('table').addClass('ui very basic celled centered table');
 
