12a26a44581c8eaa8524405042f95c8df73a2ef4
Sushovan Majhi <sushovan4@users.noreply.github.com>
2023-02-04 19:57:16 +0000

    add

--- js/script.js
+++ js/script.js
@@ -4,7 +4,12 @@ $( document ).ready(function() {
             trigger: '.title button'
         }
     });
-
+    $('.concepts.button').click(function(){
+        $('.ui.concepts.modal')
+         .modal('show')
+        ;
+    });
     $('.ui.dropdown').dropdown();
     $('table').addClass('ui very basic celled centered table');  
+    
 });
